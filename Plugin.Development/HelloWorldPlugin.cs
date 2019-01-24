32c42b61ec62062bbead271b757a52b9b968f40a
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-01-24 15:06:32 +0530

    Hello World CRM plugin

---
+++ Plugin.Development/HelloWorldPlugin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Development
+{
+    public class HelloWorldPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            
+            Entity entity = (Entity)context.InputParameters["Target"];
+            string name = (string)entity["ita_name"];
+            if ("Test".Equals(name))
+            {
+                throw new InvalidPluginExecutionException("Cannot use this name");
+            }
+
+            //Organizational service 
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+        }
+    }
+}
