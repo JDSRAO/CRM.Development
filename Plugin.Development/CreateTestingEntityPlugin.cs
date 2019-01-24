06fa72a368bea016470d34da9143894e65665809
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-01-24 20:12:26 +0530

    Add entity plugin

---
+++ Plugin.Development/CreateTestingEntityPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Development
+{
+    public class CreateTestingEntityPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IExecutionContext)serviceProvider.GetService(typeof(IExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            Entity entity = new Entity("testing");
+            service.Create(entity);
+        }
+    }
+}
